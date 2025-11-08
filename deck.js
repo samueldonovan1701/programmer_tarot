f06b2efd860aa374f946bb22680e2183d68e223d
Samuel S. Donovan <samuel.donovan1701@gmail.com>
2025-11-07 17:38:23 -0700

    Create deck.js
    0x00

---
+++ deck.js
@@ -0,0 +1,15 @@
+const deck = [
+/* MAJOR ARCANA */      VVVVVVVVVVVVVVVVVVVVVVVVV
+ {
+  "id": "0x00",
+  "title": "0x00",
+  "name": "The New Hire",
+  "description": """The Fool card is numbered 0, which is considered to be a number of infinite potential. Consider him a blank slate, for The Fool has yet to develop a clear personality. He is the symbol of innocence - his journey to come will shape his character yet.\nTo see the The Fool generally means a beginning of a new journey, one where you will be filled with optimism and freedom from the usual constraints in life. When we meet him, he approaches each day as an adventure, in an almost childish way. He believes that anything can happen in life and there are many opportunities that are lying out there, in the world, waiting to be explored and developed. He leads a simple life, having no worries, and does not seem troubled by the fact that he cannot tell what he will encounter ahead.",
+  "upright_keywords": ["beginnings", "freedom", "innocence", "originality", "adventure", "idealism", "spontaneity"],
+  "reverse_keywords": ["reckless", "careless", "distracted", "naive", "foolish", "gullible", "stale", "dull"],
+  "upright_meaning": "The Fool card is numbered 0, which is considered to be a number of infinite potential. Consider him a blank slate, for The Fool has yet to develop a clear personality. He is the symbol of innocence - his journey to come will shape his character yet.\nTo see the The Fool generally means a beginning of a new journey, one where you will be filled with optimism and freedom from the usual constraints in life. When we meet him, he approaches each day as an adventure, in an almost childish way. He believes that anything can happen in life and there are many opportunities that are lying out there, in the world, waiting to be explored and developed. He leads a simple life, having no worries, and does not seem troubled by the fact that he cannot tell what he will encounter ahead.",
+  "reverse_meaning": "When you land on the reversed Fool in your reading, you can generally find his more negative characteristics being on display. It can mean that you are literally acting like a fool by disregarding the repercussions of your actions. Like the youth depicted in the card, you don't see how dangerous of a position you find yourself in.\nA reversed Fool card can show that you are living in the moment and not planning for the future. The reversed Fool meaning serves as caution that you should be more aware so as not to be taken advantage of. Like the dog in the Rider Waite version, this card is here to alert you on anything that may sound too good to be true."
+ },
+
+
+]
